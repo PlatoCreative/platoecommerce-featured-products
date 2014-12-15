0644c8fd1a75c142dde2340f02bb7c1ea56eec56
Will <will@platocreative.co.nz>
2014-12-16 10:57:34 +1300

    initial build

---
+++ code/decorators/FeaturedProduct.php
@@ -0,0 +1,28 @@
+<?php
+/*
+*	FeaturedProduct extends Product
+*/
+class FeaturedProduct extends DataExtension {	
+	private static $db = array(
+		'FeaturedProduct' => 'Boolean'
+	);
+	
+	public function updateCMSFields(FieldList $fields){
+		$fields->addFieldToTab('Root.Main', CheckboxField::create('FeaturedProduct', 'Is a featured product?', 0));
+		
+		return $fields;
+	}
+}
+
+/*
+*	FeaturedProduct_Controller extends Page_Controller
+*/
+class FeaturedProduct_Controller extends DataExtension {	
+	private static $allowed_actions = array(
+	);
+	
+	public function getFeaturedProducts($limit = 6, $sort = 'RAND()'){
+		$featuredProducts = Product::get()->filter(array('FeaturedProduct' => 1))->sort('$sort')->limit($limit);
+		return $featuredProducts;
+	}
+}
