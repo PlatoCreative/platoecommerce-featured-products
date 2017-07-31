e8455d86baec701a55f43febdfea6201fc3519a1
Will Dawber <will@platocreative.co.nz>
2017-08-01 11:57:41 +1200

    Update FeaturedProduct.php

--- code/decorators/FeaturedProduct.php
+++ code/decorators/FeaturedProduct.php
@@ -15,16 +15,3 @@ class FeaturedProduct extends DataExtension {
 		return $fields;
 	}
 }
-
-/*
-*	FeaturedProduct_Controller extends Page_Controller
-*/
-class FeaturedProduct_Controller extends DataExtension {	
-	private static $allowed_actions = array(
-	);
-	
-	public function getFeaturedProducts($limit = 6, $sort = 'RAND()'){
-		$featuredProducts = Product::get()->filter(array('FeaturedProduct' => 1))->sort($sort)->limit($limit);
-		return $featuredProducts;
-	}
-}
